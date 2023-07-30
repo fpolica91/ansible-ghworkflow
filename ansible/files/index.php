dcecdb177036dfdcba506aeb3f2cd1b80c83c338
Fabricio <fabriciopolicarpo0@gmail.com>
2023-07-29 22:41:41 -0400

    [ds

--- ansible/files/index.php
+++ ansible/files/index.php
@@ -1,1 +1,14 @@
-<?php echo "Hello World"; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My PHP Page</title>
+    </head>
+    <body>
+        <h1>Welcome to my PHP page!</h1>
+
+        <?php
+            echo "<p>Hello, world! Today is " . date('Y-m-d H:i:s') . "</p>";
+        ?>
+
+    </body>
+</html>
